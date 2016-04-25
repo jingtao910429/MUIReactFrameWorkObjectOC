833c4ea8f64f66f54071093ac016ecc1b5e4aeec
2252819957 <wei.wutao@rongyu100.com>
2016-04-25 11:29:51 +0800

    项目导入

---
+++ MUIReactFrameWorkObjectOC/MUIReactFrameWorkObjectOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MUIReactFrameWorkObjectOC
+//
+//  Created by wwt on 16/4/25.
+//  Copyright © 2016年 rongyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MUIReactFrameWorkObjectOC/MUIReactFrameWorkObjectOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MUIReactFrameWorkObjectOC
+//
+//  Created by wwt on 16/4/25.
+//  Copyright © 2016年 rongyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
